66235bbe80d58a1b878eb76edefaa4abe6cecf6d
rmaru <rmarunchak@teladoc.com>
2023-01-26 13:40:25 +0200

    singleton

--- features/step_definitions/verify_steps.rb.rb
+++ features/step_definitions/verify_steps.rb.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 And(/^I verify Bank Wire order$/) do
-  page = Elopage::BankWirePage.new
   sleep 3
   page.save_screenshot('screen.png', full: true)
-  page.verify_thank_you_text
+  PageFactory.bank_wire_page.verify_thank_you_text
 end
